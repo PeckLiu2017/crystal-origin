c2a90b7b0d658d967e5eb2fd9f6166f89c3563e5
peck liu <xiaoyang20170204@gmail.com>
2017-10-15 23:34:59 +0800

    use eventmachine

--- crystal.rb
+++ crystal.rb
@@ -5,6 +5,7 @@ require 'socket'
 require 'http/parser'
 require 'stringio'
 require 'thread'
+require 'eventmachine'
 
 class Crystal
   # 通过 port 号建立一个 TCPServer socket
@@ -15,6 +16,10 @@ class Crystal
 
   def prefork(workers)
     workers.times do
+      # 基本上与:: new相同
+      # 但是，如果使用 fork 方法创建子进程
+      # 则在该子类中调用start将不会调用子类的initialize方法
+      # 子类会与父类共享 @socket
       fork do
         puts "Forked #{Process.pid}"
         start
@@ -37,6 +42,14 @@ class Crystal
     end
   end
 
+  def start_em
+    EM.run do
+      EM.start_server "localhost", 3000, EMConnection do |connection|
+        connection.app = @app
+      end
+    end
+  end
+
   class Connection
     def initialize(socket, app)
       @socket = socket
@@ -44,6 +57,8 @@ class Crystal
       @parser = Http::Parser.new(self)
     end
 
+    # process 解析请求
+    # 组成请求报文加入 @parser 数组中
     def process
       # read 1024 bytes
       # 但数据有比 1024 bytes 多的情况
@@ -99,6 +114,63 @@ class Crystal
     end
   end
 
+
+  class EMConnection < EM::Connection
+    attr_accessor :app
+
+    # Http::Parser 解析请求
+    def post_init
+      @parser = Http::Parser.new(self)
+    end
+
+    # 将 Http::Parser 解析的请求
+    # 组成请求报文加入 @parser 数组中
+    def receive_data(data)
+      @parser << data
+    end
+
+    # 一旦 request 被收到执行回调函数
+    def on_message_complete
+      # puts "#{@parser.http_method} #{@parser.request_path}"
+      # puts "   " + @parser.headers.inspect
+      # puts
+
+      env = {}
+      @parser.headers.each do |name, value|
+        name = "HTTP_" + name.upcase.tr("_","-")
+        env[name] = value
+      end
+      env["PATH_INFO"] = @parser.request_path
+      env["REQUEST_METHOD"] = @parser.http_method
+      env["rack.input"] = StringIO.new
+      send_response env
+    end
+
+    REASONS = {
+      200 => "OK",
+      404 => "NOT FOUND"
+    }
+    # 因为可能有 another one？
+    def send_response(env)
+      status, headers, body = @app.call(env)
+      reason = REASONS[status]
+      # "\r\n" 一般一起用，表示键盘上的回车键
+      send_data "HTTP/1.1 #{status} #{reason}\r\n"
+      headers.each_pair do |name, value|
+        send_data "#{name}: #{value}\r\n"
+      end
+      send_data "\r\n"
+      body.each do |chunk|
+        send_data chunk
+      end
+
+      body.close if body.respond_to? :close
+
+      close_connection_after_writing
+    end
+
+  end
+
   # 重构 class App
   class Builder
     attr_reader :app
@@ -123,4 +195,5 @@ app = Crystal::Builder.parse_file('config.ru')
 server = Crystal.new(3000, app)
 puts "Plugging crystal into port 3000"
 # server.start
-server.prefork(3)
+# server.prefork(3)
+server.start_em
